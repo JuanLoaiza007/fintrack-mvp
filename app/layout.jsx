53115acf8903dbe43f17afc1f80746666c17dc8e
juanloaiza007 <juandaloaiza723@gmail.com>
2025-03-18 13:10:18 -0500

    feat(TransactionForm): integrate context and schema options

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+import { TransactionProvider } from "@/context/TransactionContext";
 
 export const metadata = {
   title: "FinTrack",
@@ -8,7 +9,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
-      <body className="flex w-full h-full">{children}</body>
+      <TransactionProvider>
+        <body className="flex w-full h-full">{children}</body>
+      </TransactionProvider>
     </html>
   );
 }
